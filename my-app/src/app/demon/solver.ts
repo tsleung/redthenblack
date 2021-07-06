f172fa1b9ec89765e40eb096f1c2c237dda94b20
Terran <terran.leung@gmail.com>
2021-07-06 14:27:30 -0400

    Removed whitespace, testing token

---
+++ my-app/src/app/demon/solver.ts
@@ -0,0 +1,29 @@
+
+interface ValueResult {
+    value: number;
+    runSamples: [],
+    allRuns: []
+}
+// solve for value, given time and confidence
+function solveValue(numPeriods, threshold) {
+
+    return {
+        value: 0,
+        runSamples: [],
+        allRuns: []
+    }
+}
+
+function solveConfidence(value, numPeriods) {
+    // given 
+
+}
+
+
+
+
+// may not be able to solve!
+function solveTime(value, threshold) {
+    // 
+
+}
