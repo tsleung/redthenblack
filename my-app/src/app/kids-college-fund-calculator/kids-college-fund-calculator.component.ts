14df05c83f25d30735f5213a84646a2cd5546154
Terran <terran@Terrans-MacBook-Air.local>
2023-07-24 01:04:16 -0700

    Added bold, rearranged calculator before article

--- my-app/src/app/kids-college-fund-calculator/kids-college-fund-calculator.component.ts
+++ my-app/src/app/kids-college-fund-calculator/kids-college-fund-calculator.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+/**
+ * Can you write a web article that takes 10 minutes to read for an undergraduate which describes the quantitative approach to saving for a kid's college fund.
+ */
+
 @Component({
   selector: 'app-kids-college-fund-calculator',
   templateUrl: './kids-college-fund-calculator.component.html',
